c44ffcfca3495809b0c627aae01df7a6589f43a2
Dan Bryce <admin@danjb.com>
2019-04-13 22:47:49 +0100

    Add Image class

--- texture-builder/texture-builder.cpp
+++ texture-builder/texture-builder.cpp
@@ -10,7 +10,40 @@
 
 #include "stb_image.h"
 
-unsigned char* loadImage(std::string filename) {
+const int TEXTURE_WIDTH = 2048;
+const int TEXTURE_HEIGHT = 2048;
+
+class Image {
+
+private:
+
+    int width;
+    int height;
+    unsigned char *data;
+
+public:
+
+    Image(int width, int height, unsigned char *data) {
+        this->width = width;
+        this->height = height;
+        this->data = data;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    unsigned char* getData() {
+        return data;
+    }
+
+};
+
+Image loadImage(std::string filename) {
     std::cout << "Loading: " << filename << "\n";
 
     int width, height, n;
@@ -20,16 +53,24 @@ unsigned char* loadImage(std::string filename) {
     if (data == NULL) {
         printf("Failed to load image!\n");
         printf("Error: %s\n", stbi_failure_reason());
+        throw std::runtime_error("Error loading image");
     }
 
-    return data;
+    return Image(width, height, data);
 }
 
 int main() {
+
+    // Create an empty texture
+    unsigned char *data = new unsigned char[TEXTURE_WIDTH * TEXTURE_HEIGHT];
+    Image texture = Image(TEXTURE_WIDTH, TEXTURE_HEIGHT, data);
+
+    // Load our definition file
     std::ifstream file("definitions/unit_human_knight.def");
     std::string line;
+
     while (std::getline(file, line)) {
-        unsigned char *data = loadImage("images/" + line);
+        Image sprite = loadImage("images/" + line);
         // TODO: add image to texture
         // TODO: start a new row once we hit the max image size
     }
