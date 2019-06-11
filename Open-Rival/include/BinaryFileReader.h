69d826c640f1309fef372835975710bbf0735ccc
Dan Bryce <admin@danjb.com>
2019-06-11 23:25:35 +0100

    Improve image loading (WIP); move Window destruction to destructor

---
+++ Open-Rival/include/BinaryFileReader.h
@@ -0,0 +1,32 @@
+#ifndef BINARY_FILE_READER_H
+#define BINARY_FILE_READER_H
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+namespace Rival {
+
+    class BinaryFileReader {
+
+    private:
+
+        std::ifstream in;
+
+    public:
+
+        BinaryFileReader(const std::string filename);
+
+        void skip(int n);
+
+        std::uint8_t readByte();
+
+        std::uint16_t readShort();
+
+        void read(std::vector<unsigned char>* buffer);
+
+    };
+
+}
+
+#endif // BINARY_FILE_READER_H
--- Open-Rival/include/Window.h
+++ Open-Rival/include/Window.h
@@ -13,7 +13,7 @@ namespace Rival {
 
         Window(const int width, const int height, const char* title);
 
-        void destroy();
+        ~Window();
 
         void use();
 
