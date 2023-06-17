2dbf91fe065534835094d7a91c65636e217f155b
Ricardo Benítez <benitezc.ricardo@gmail.com>
2023-06-17 21:21:27 +0200

    build: update create vs solution bat script (#10)
    Signed-off-by: Ricardo Benítez <benitezc.ricardo@gmail.com>

--- create_vs_solution.bat
+++ create_vs_solution.bat
@@ -5,8 +5,11 @@ set "BUILD_TYPE=%~1"
 if "%BUILD_TYPE%"=="" set "BUILD_TYPE=debug"
 if /I "%BUILD_TYPE%" neq "debug" if /I "%BUILD_TYPE%" neq "release" set "BUILD_TYPE=debug"
 
+set "PLATFORM=%~2"
+if /I "%PLATFORM%" neq "x64" if /I "%PLATFORM%" neq "Win32" set "PLATFORM=Win32"
+
 set "SOURCE_DIR=%~dp0"
-set "BUILD_DIR=%SOURCE_DIR%\cmake-build-%BUILD_TYPE%"
+set "BUILD_DIR=%SOURCE_DIR%\cmake-build-%BUILD_TYPE%-%PLATFORM%"
 
 REM Create the build directory
 mkdir "%BUILD_DIR%"
@@ -15,5 +18,5 @@ REM Change to the build directory
 cd /D "%BUILD_DIR%"
 
 REM Configure the CMake project
-cmake  -A Win32 ..
+cmake -A "%PLATFORM%" ..
 cd ..
